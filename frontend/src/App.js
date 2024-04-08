b25344c9ed5f43e7382ea815d89610d6c54f66f1
Gautham P <gauthampdn@gmail.com>
2024-04-08 15:08:43 -0700

    all

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Ensure you import the CSS file for additional styles if needed
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className=' bg-black'>
+      <div className="relative h-screen w-screen ">
+        <div
+          className="absolute top-0 left-0 h-full w-full bg-center bg-cover"
+          style={{ backgroundImage: `url(frame.jpg)` }}
+        ></div>
+
+        {/* Gradient overlay starting halfway down */}
+        <div className="absolute top-2/3 left-0 h-1/3 w-full bg-gradient-to-b from-transparent to-black"></div>
+
+        {/* Text on the bottom right */}
+
+        <img className="absolute top-0 m-2 mt-8 w-28 h-28" src="initial.png" />
+
+        <div className="absolute bottom-0 left-0 p-8">
+          <h1 className="text-white text-3xl font-extrabold">Gautham<br />Pandian</h1>
+          <h1 className="pt-4 text-white text-md font-semibold"> Hey! I'm Gautham, documenting my journey through fitness and
+        stuff I read to keep me accountable - so philosophical ponderings with a side push-ups.</h1>
+        </div>
+      </div>
+
+
+      <h1 className="p-8 text-white text-3xl font-extrabold">Links:</h1>
+
+      <div className="grid grid-cols-2 gap-4 pr-8 pl-8 pb-20">
+        <a href="https://www.instagram.com/gauthampdn" target="_blank" rel="noopener noreferrer" className="w-full border-2 rounded-3xl p-2">
+          <div className="w-full h-50 bg-black rounded-3xl shadow-lg hover:shadow-2xl transition-shadow flex items-center justify-center">
+            <img
+              src="/insta.png"
+              alt="Instagram"
+              className="w-1/5" // This sets the image height to 75% of the container height
+            />
+          </div>
+          <h1 className='pt-2 text-white text-sm font-semibold flex justify-center'>@gauthampdn</h1>
+
+        </a>
+
+        <a href="https://www.tiktok.com/@gauthampdn" target="_blank" rel="noopener noreferrer" className="w-full border-2 rounded-3xl p-2">
+          <div className="w-full h-50 bg-black rounded-3xl shadow-lg hover:shadow-2xl transition-shadow flex items-center justify-center">
+            <img
+              src="/tiktok.png"
+              alt="TikTok"
+              className="w-1/5" // This sets the image height to 75% of the container height
+            />
+          </div>
+          <h1 className='pt-2 text-white text-sm font-semibold flex justify-center'>gauthampdn</h1>
+
+        </a>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+/*
+
+      
+      */
